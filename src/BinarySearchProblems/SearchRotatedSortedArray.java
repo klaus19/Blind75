8e17c68afc7299f660f99cb6ae7f2fed4005eb96
tejaskhartude <tejaskhartude@gmail.com>
2022-08-21 11:51:01 +0530

    Initial commit

---
+++ src/BinarySearchProblems/SearchRotatedSortedArray.java
@@ -0,0 +1,29 @@
+package BinarySearchProblems;
+
+
+//Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums,
+// or -1 if it is not in nums.
+//
+//You must write an algorithm with O(log n) runtime complexity.
+
+public class SearchRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+
+        int start = 0, end = nums.length- 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+                else start = mid + 1;
+            }
+            else {
+                if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+}
