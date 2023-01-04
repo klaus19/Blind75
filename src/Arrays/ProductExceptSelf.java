3c18979e665912073e9a0e2091acd0f7ffb913d4
klaus19 <tejaskhartude@gmail.com>
2023-01-04 12:00:00 +0530

    Added new problm

--- src/Arrays/ProductExceptSelf.java
+++ src/Arrays/ProductExceptSelf.java
@@ -3,35 +3,36 @@ package Arrays;
 //Given an integer array nums, return an array answer such that
 // answer[i] is equal to the product of all the elements of nums except nums[i].
 
+import java.util.Arrays;
+
 public class ProductExceptSelf {
 
 
     public int[] productExceptSelf(int[] nums) {
 
-        int N = nums.length;
+        int output[] = new int[nums.length];
 
-      int[]L = new int[N];
-      int[]R = new int[N];
-      int pointer=1;
-      int[]output_array = new int[N];
+        output[0]=1;
 
-      L[0]=1;
-      R[N-1]=1;
+        // left prefix product
+        for (int i=1;i<nums.length;i++){
+            output[i]=output[i-1]*nums[i-1];
 
-      for (int i=1;i<N;i++){
-          L[i]=nums[i-1]*L[i-1];
+        }
+        int product=1;
+        for(int i=nums.length-1;i>=0;i--){
 
-      }
+            output[i] = output[i] * product;
 
-      for (int i=N-2;i>=0;i--){
-          R[i]=nums[i+1]*R[i+1];
-      }
+            product*= nums[i];
+        }
+        return output;
 
-      for (int i=0;i<N;i++){
-          output_array[i]=L[i]*R[i];
-      }
+    }
 
-      return output_array;
-
+    public static void main(String[] args) {
+        ProductExceptSelf pd = new ProductExceptSelf();
+        int [] pt = new int[]{2,3,4};
+        System.out.println(Arrays.toString(pd.productExceptSelf(pt)));
     }
 }
